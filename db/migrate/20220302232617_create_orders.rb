447271716a36b2900863089465cadafd27fc1fc7
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-03 06:39:55 -0300

    refatorando tabela order e serializer

--- db/migrate/20220302232617_create_orders.rb
+++ db/migrate/20220302232617_create_orders.rb
@@ -2,9 +2,10 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.float :value_total
+      t.datetime :date_order
       t.references :user, null: false, foreign_key: true
-      t.references :product, null: false, foreign_key: true
 
+      t.datetime :deleted_at, index: true
       t.timestamps
     end
   end
