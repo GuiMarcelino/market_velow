b5f57b94e624e59d8419bc07ad04b2dc006f1c48
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-03-06 20:25:11 -0300

    add coluna valor total em pedidos

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
-  belongs_to :product
+  has_many :products
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_06_231054) do
+ActiveRecord::Schema.define(version: 2022_03_06_232140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_06_231054) do
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "value_total"
     t.index ["active"], name: "index_orders_on_active"
     t.index ["created_by_id"], name: "index_orders_on_created_by_id"
     t.index ["deleted_at"], name: "index_orders_on_deleted_at"
